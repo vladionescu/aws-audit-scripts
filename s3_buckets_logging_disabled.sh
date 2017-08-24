f3e83c190adc7a6041417a29ca91c93506ae47b1
Vlad Ionescu <vlad.ionescu@nccgroup.trust>
2017-08-23 20:42:18 -0700

    Added check for S3 buckets without logging

---
+++ s3_buckets_logging_disabled.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Accepts newline delimited AWS profiles from STDIN (piped input works well).
+PROFILES=$(cat -)
+
+# For each given profile, find if any of the S3 buckets don't have access logging.
+# Outputs in the following format:
+# <profile> <s3 bucket>
+for PROFILE in $PROFILES; do
+    for BUCKET in $(aws --profile ${PROFILE} s3api list-buckets | jq -r '.Buckets[].Name'); do
+        LOGGING_POLICY=$(aws --profile ${PROFILE} s3api get-bucket-logging --bucket ${BUCKET})
+    
+        if [ -z "${LOGGING_POLICY}" ]; then
+            echo "$PROFILE $BUCKET $LOGGING_POLICY"
+        fi
+    done
+done
