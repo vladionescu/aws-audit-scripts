bca8eeb52b5f90f9c5733f528943cbe74acb0735
Vlad Ionescu <vlad.ionescu@nccgroup.trust>
2017-08-25 09:50:37 -0700

    Updated min password policy to report if the account lacks a password policy

--- minimum_password_length.sh
+++ minimum_password_length.sh
@@ -3,12 +3,22 @@
 PROFILES=$(cat -)
 
 # For each given profile, print the minimum password length as set by the password policy.
+# If the account does not have a password policy set, <min length> will be "no-password-policy".
 # Outputs in the following format:
 # <profile> <min length>
 for PROFILE in $PROFILES; do
-    LENGTH=$(aws --profile ${PROFILE} iam get-account-password-policy | jq -r '.PasswordPolicy.MinimumPasswordLength')
+    rm ./.pw-policy-err.tmp 2>/dev/null
+    LENGTH=$( aws --profile ${PROFILE} iam get-account-password-policy 2>./.pw-policy-err.tmp | jq -r '.PasswordPolicy.MinimumPasswordLength')
+
+    AWSERR=$(cat ./.pw-policy-err.tmp)
+
+    if [ ! -z "${AWSERR}" ] && [[ "${AWSERR}" == *"NoSuchEntity"* ]]; then
+        LENGTH="no-password-policy"
+    fi
 
     if [ ! -z "${LENGTH}" ]; then
         echo "$PROFILE $LENGTH"
     fi
+
 done
+rm ./.pw-policy-err.tmp 2>/dev/null
